2a9deb3f49d01e52e7ceb0b274528b4d37317e8e
Loïc de la Tullaye <ldelatullaye@juniorisep.com>
2025-01-06 15:44:48 +0100

    feat: rework style

--- app/src/main/java/com/project/fitnessbuddy/screens/auth/RegisterScreen.kt
+++ app/src/main/java/com/project/fitnessbuddy/screens/auth/RegisterScreen.kt
@@ -1,5 +1,6 @@
 package com.project.fitnessbuddy.screens.auth
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
@@ -25,45 +26,51 @@ fun RegisterScreen(
     var confirmPassword by remember { mutableStateOf("") }
     val userState by authViewModel.userState.collectAsState()
 
-    Column(
+    Box(
         modifier = Modifier
             .fillMaxSize()
-            .padding(16.dp),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
+            .background(MaterialTheme.colorScheme.background)
     ) {
-        Text(stringResource(id = R.string.register), style = MaterialTheme.typography.headlineMedium)
-        Spacer(modifier = Modifier.height(16.dp))
-        TextField(value = email, onValueChange = { email = it }, label = { Text(stringResource(id = R.string.email)) })
-        Spacer(modifier = Modifier.height(8.dp))
-        TextField(
-            value = password,
-            onValueChange = { password = it },
-            label = { Text(stringResource(id = R.string.password)) },
-            visualTransformation = PasswordVisualTransformation()
-        )
-        Spacer(modifier = Modifier.height(8.dp))
-        TextField(
-            value = confirmPassword,
-            onValueChange = { confirmPassword = it },
-            label = { Text(stringResource(id = R.string.confirm_password)) },
-            visualTransformation = PasswordVisualTransformation()
-        )
-        Spacer(modifier = Modifier.height(16.dp))
-        Button(onClick = {
-            authViewModel.register(context, email, password, confirmPassword)
-        }) {
-            Text(stringResource(id = R.string.register))
-        }
-        Spacer(modifier = Modifier.height(8.dp))
-        TextButton(onClick = {
-            navController.navigate("login")
-        }) {
-            Text(stringResource(id = R.string.already_have_account))
-        }
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(16.dp),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(stringResource(id = R.string.register), style = MaterialTheme.typography.headlineMedium, color = MaterialTheme.colorScheme.primary)
+            Spacer(modifier = Modifier.height(16.dp))
+            TextField(value = email, onValueChange = { email = it }, label = { Text(stringResource(id = R.string.email)) })
+            Spacer(modifier = Modifier.height(8.dp))
+            TextField(
+                value = password,
+                onValueChange = { password = it },
+                label = { Text(stringResource(id = R.string.password)) },
+                visualTransformation = PasswordVisualTransformation()
+            )
+            Spacer(modifier = Modifier.height(8.dp))
+            TextField(
+                value = confirmPassword,
+                onValueChange = { confirmPassword = it },
+                label = { Text(stringResource(id = R.string.confirm_password)) },
+                visualTransformation = PasswordVisualTransformation()
+            )
+            Spacer(modifier = Modifier.height(16.dp))
+            Button(onClick = {
+                authViewModel.register(context, email, password, confirmPassword)
+            }) {
+                Text(stringResource(id = R.string.register))
+            }
+            Spacer(modifier = Modifier.height(8.dp))
+            TextButton(onClick = {
+                navController.navigate("login")
+            }) {
+                Text(stringResource(id = R.string.already_have_account))
+            }
 
-        if (userState.isLoggedIn) {
-            onRegisterSuccess()
+            if (userState.isLoggedIn) {
+                onRegisterSuccess()
+            }
         }
     }
 }
